a14cffb5ab6f8e0670c74bd3b422588a2919ebac
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2013-09-12 14:39:41 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cems::Application.config.secret_key_base = '3afb0452acca684515c22d2625726d718a48acb6185331b68025075fd3f33b2b73c808b3ad107a4a0c53863b8339d6ddca500cf03bca582316a8df9aef4646dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cems::Application.config.session_store :cookie_store, key: '_cems_session'
