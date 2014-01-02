9739a23a8dce45ae879a050199048b8cef945aec
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-01-02 20:52:15 -0300

    fix specs so auth is disabled on test env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :check_authentication
+  before_action :check_authentication unless Rails.env.test?
 end
