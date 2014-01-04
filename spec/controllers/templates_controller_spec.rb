3f7a6382e74eaaeeff00031d0423401946143473
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-01-04 17:12:40 -0300

    implement template controller spec

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -1,12 +1,8 @@
 require 'spec_helper'
 
 describe TemplatesController do
-
-  describe "GET 'template'" do
-    it "returns http success" do
-      get 'template'
-      response.should be_success
-    end
+  it "should return the dashboard template" do
+    get 'template', { path: 'dashboard' }
+    response.should be_success
   end
-
 end
