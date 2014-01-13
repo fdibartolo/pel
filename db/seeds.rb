7e3229212be36f6d593ec8f4c40ea2699443079f
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-01-13 14:51:02 -0300

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def user_exists? eid
+  User.find_by(enterprise_id: eid)
+end
+
+%W[
+  fernando.di.bartolo
+  heraldo.gimenez
+].each do |enterprise_id|
+  User.create! enterprise_id: enterprise_id unless user_exists? enterprise_id
+end
