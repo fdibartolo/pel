75e0ebada14ba975322bf971f358853075aa6328
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-01-24 20:48:26 -0300

    add spec for templates and refactor it

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe TemplatesController do
-  it "should return the dashboard template" do
-    get 'template', { path: 'dashboard' }
-    response.should be_success
-    response.should render_template 'dashboard'
+  %w[
+    dashboard
+    new
+  ].each do |template|
+    it "should return the #{template} template" do
+      get 'template', { path: template }
+      response.should be_success
+      response.should render_template template
+    end
   end
 end
