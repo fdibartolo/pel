87add507830c73d4629ff0846f7eb3a49634cb75
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2014-01-24 21:01:42 -0300

    add spec for app signout

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
 describe ApplicationController do
+  before :each do
+    @user = FactoryGirl.create :user
+  end
+
+  def valid_session
+    { enterprise_id: @user.enterprise_id }
+  end
+
   it "should return success (to be called by CI box to check app running)" do
     get :health
     response.should be_success
+  end
+
+  it "should be able to signout" do
+    get :signout, valid_session
+    session[:enterprise_id].should be_nil
+    response.should redirect_to logout_path
   end
 end
